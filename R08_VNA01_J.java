edbc48296897e00fdf17e559e5cb84698d27a987
Haru5164 <116036081+Haru5164@users.noreply.github.com>
2022-10-17 18:20:51 -0400

    Create R08_VNA01_J.java

---
+++ R08_VNA01_J.java
@@ -0,0 +1,12 @@
+
+final class Foo {
+  private Helper helper;
+ 
+  public Helper getHelper() {
+    return helper;
+  }
+ 
+  public void setHelper(int num) {
+    helper = new Helper(num);
+  }
+}
